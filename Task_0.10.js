9123457589f5bf706de9f56820391d9bc08782b3
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-06-15 13:57:22 +0200

    Update Task_0.10.js

--- Task_0.10.js
+++ Task_0.10.js
@@ -1,10 +1,12 @@
 function commonLetters(string_1, string_2) {
     var common = "";
     string_1, string_2.toLowerCase();
+    //Iterate over characters in string1
     for (const i of string_1){
+        //Iterate over characters in string2
         for (const x of string_2){
             if (i === x){
-                common += x;
+                common +=" " + x;
             }
         }
     }
