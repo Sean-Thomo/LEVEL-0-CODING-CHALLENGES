88befc316602c77277cbb000b7cdc023ee8ad83b
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-06-15 13:54:51 +0200

    Update Task_0.8.js

--- Task_0.8.js
+++ Task_0.8.js
@@ -2,7 +2,25 @@ function numberToHoursAndMinutes(number){
     var hours,minutes;
     hours = Math.floor(number /60);
     minutes = number % 60;
-    console.log(hours +" hour(s), "+ minutes +(" minute(s)."))
+    if (hours > 1 && minutes > 1) {
+        console.log(hours +" hours, "+ minutes +" minutes.")
+    }else if(hours > 1 && minutes == 1){
+        console.log(hours +" hours, "+ minutes +" minute.")
+    }else if(hours == 1 && minutes > 1){
+        console.log(hours +" hour, "+ minutes +" minutes.")
+    }else if(hours > 1 && minutes == 0){
+        console.log(hours  + " hours ")
+    }else if(hours == 0 && minutes == 1) {
+        console.log(minutes +" minute" )
+    }else if(hours == 0 && minutes > 1) {
+        console.log(minutes + " minutes")
+    }   
+    else{
+        console.log(hours+ " hour")
+    }
 }
 
-numberToHoursAndMinutes(578);
+numberToHoursAndMinutes(71);
+numberToHoursAndMinutes(133);
+numberToHoursAndMinutes(121);
+numberToHoursAndMinutes(1);
