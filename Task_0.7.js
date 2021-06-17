26cd0812c5c2d6a67f591582b8e6fa77b2f3e54a
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-06-17 20:19:07 +0200

    Update Task_0.7.js
    updated the function so that it returns the results rather than print them out

--- Task_0.7.js
+++ Task_0.7.js
@@ -1,11 +1,11 @@
 function celsiusToFahrenheit(temp) {
-    var f = (temp * 9/5) + 32
-    console.log(f)
+    var f = (temp * 9/5) + 32;
+    return f;
 };
 
 function fahrenheitToCelsius(temp) {
     var c = (temp -32 ) * 5/9;
-    console.log(c)
+    return c;
 }
 
 celsiusToFahrenheit(0);
