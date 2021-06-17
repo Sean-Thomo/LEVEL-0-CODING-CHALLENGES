9b99c068ce7f3a898112afd84e05de603c30daf2
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-06-17 20:01:05 +0200

    Update Task_0.10.js
    Updated the code so that it removes any duplicate letters from "output", and add commas after letters.

--- Task_0.10.js
+++ Task_0.10.js
@@ -1,15 +1,18 @@
 function commonLetters(string_1, string_2) {
     var common = "";
+    var output = "";
     string_1, string_2.toLowerCase();
     //Iterate over characters in string1
     for (const i of string_1){
         //Iterate over characters in string2
         for (const x of string_2){
             if (i === x){
-                common +=" " + x;
+                output += x + "," ;
             }
         }
     }
+    //remove any duplicate letters from "output"
+    common = Array.from(new Set(output.split(','))).toString();
     console.log("Common letters: " + common)
 }
 commonLetters('house', 'computers');
