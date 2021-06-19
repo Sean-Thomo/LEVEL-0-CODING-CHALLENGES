109435cfd56e2acfead2925d87af889c9e7309ae
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-06-19 10:55:39 +0200

    Update Task_0.9.js
    removed toString and used `.join(", ")`. Also fixed the output to match the expected output.

--- Task_0.9.js
+++ Task_0.9.js
@@ -2,16 +2,15 @@ function printVowels(string){
   var str,vowelsFound, vowelList;
   str = string.toLowerCase();
   vowelList = ['a', 'e', 'i', 'o', 'u'];
-  vowelsFound = "";
-
+  vowelsFound ="";
   //Iterate over characters in the string
   for (const x of str) {
     if (vowelList.indexOf(x) !== -1) {
-      vowelsFound +=x + ", "
+      vowelsFound +=x;
     }
   }
-  vowelsFound = Array.from(new Set(vowelsFound.split(', '))).toString();
-  console.log("Vowels :" + vowelsFound);
+  vowelsFound = Array.from(new Set(vowelsFound)).join(", ");
+  console.log("vowels: " + vowelsFound);
 }
 
-printVowels("Umuzi")
+printVowels("Umuzi");
