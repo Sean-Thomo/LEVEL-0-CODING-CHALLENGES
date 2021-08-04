a7aed306a6997062a8f574719824dd8a6bb7d5c4
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-08-04 12:52:42 +0200

    Update Task_0.5.js
    removed comments.

--- Task_0.5.js
+++ Task_0.5.js
@@ -1,9 +1,7 @@
 function areaOfTriangle(a, b, c) {
-    //Calculating the SemiPerimeter
     var semiPerimeter = 1/2 *(a + b + c);
     var s, area;
     s = semiPerimeter;
-    // Herons formula
     area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
     console.log(area);
 }
