f3f10524f44ace51671d0332b3a2f37bb4aa0096
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-08-04 12:58:22 +0200

    Update Task_0.10.js
    removed coments.

--- Task_0.10.js
+++ Task_0.10.js
@@ -1,17 +1,15 @@
-function commonLetters(string_1, string_2) {
-    var common = "";
-    var output = "";
-    string_1, string_2.toLowerCase();
-    //Iterate over characters in string1
+function commonLetters(stringOne, stringTwo) {
+    let common = "";
+    let output = "";
+    stringOne, stringTwo.toLowerCase();
+    
     for (const i of string_1){
-        //Iterate over characters in string2
         for (const x of string_2){
             if (i === x){
                 output += x + "," ;
             }
         }
     }
-    //remove any duplicate letters from "output"
     common = Array.from(new Set(output.split(','))).toString();
     console.log("Common letters: " + common)
 }
