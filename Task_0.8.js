d13954429c3f16a94e35db3765bdb6f25478ecac
Siphesihle Thomo <79644296+Sean-Thomo@users.noreply.github.com>
2021-08-11 16:51:08 +0200

    Update Task_0.8.js
    added another ```else if``` statement to check for 1 hour and minute

--- Task_0.8.js
+++ Task_0.8.js
@@ -6,6 +6,8 @@ function numberToHoursAndMinutes(number){
         console.log(hours +" hours, "+ minutes +" minutes.")
     }else if(hours > 1 && minutes == 1){
         console.log(hours +" hours, "+ minutes +" minute.")
+    }else if(hours == 1 && minutes == 1){
+        console.log(hours +" hour, "+ minutes +" minute.")
     }else if(hours == 1 && minutes > 1){
         console.log(hours +" hour, "+ minutes +" minutes.")
     }else if(hours > 1 && minutes == 0){
